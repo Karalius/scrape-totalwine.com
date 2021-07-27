f62087258be466281d4a7ef9baad49d50a962160
Karalius <33183813+Karalius@users.noreply.github.com>
2021-07-27 18:19:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,12 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-with open('README.rst') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-    
-
 setup(
     name='totalwine-scraper',
     version='1.0.0',
     description='This package scrapes totalwine.com website for demanded count of listings and types for wine.',
-    long_description=readme,
     author='Kasparas Karalius',
     author_email='',
     url='https://github.com/Karalius/totalwine-scraper',
-    license=license,
     packages=find_packages(exclude=('tests*')),
 )
