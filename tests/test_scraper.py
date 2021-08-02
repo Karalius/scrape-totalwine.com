4b14f843b51015233998a84352d22b06313d5136
Karalius <kasparas.karalius@gmail.com>
2021-08-02 12:40:20 +0100

    formatted code

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -4,62 +4,62 @@ import pandas as pd
 
 
 def test_number_of_items():
-    scraper = Scraper(200, ['red'])
+    scraper = Scraper(200, ["red"])
     assert scraper.number_of_items == 200
 
 
 def test_item_types():
-    scraper = Scraper(200, ['reD', 'w hItE'])
-    assert scraper.item_types == ['Red', 'White']
+    scraper = Scraper(200, ["reD", "w hItE"])
+    assert scraper.item_types == ["Red", "White"]
 
 
 def test_number_of_pages():
-    scraper = Scraper(250, ['red'])
+    scraper = Scraper(250, ["red"])
     assert scraper.number_of_pages == 2
 
 
 def test_items_to_return():
-    scraper = Scraper(250, ['red', 'white'])
+    scraper = Scraper(250, ["red", "white"])
     assert scraper.items_to_return == 500
 
 
 def test_get_headers():
-    scraper = Scraper(200, ['red'])
-    headers = scraper.get_headers(2, 'Red')
+    scraper = Scraper(200, ["red"])
+    headers = scraper.get_headers(2, "Red")
     true_headers = {
-            'authority': 'www.totalwine.com',
-            'sec-ch-ua': '^\\^',
-            'accept': 'application/json, text/plain, */*',
-            'sec-ch-ua-mobile': '?0',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-dest': 'empty',
-            'referer': f'https://www.totalwine.com/wine/c/c0020?&page=1&pageSize=200&department=Wine&producttype=Red^%^20Wine&aty=1,1,0,1',
-            'accept-language': 'en-US,en;q=0.9',
-        }
+        "authority": "www.totalwine.com",
+        "sec-ch-ua": "^\\^",
+        "accept": "application/json, text/plain, */*",
+        "sec-ch-ua-mobile": "?0",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-dest": "empty",
+        "referer": f"https://www.totalwine.com/wine/c/c0020?&page=1&pageSize=200&department=Wine&producttype=Red^%^20Wine&aty=1,1,0,1",
+        "accept-language": "en-US,en;q=0.9",
+    }
     assert headers == true_headers
 
 
 def test_get_params():
-    scraper = Scraper(200, ['Red', 'White'])
-    params = scraper.get_params(3, 'White')
+    scraper = Scraper(200, ["Red", "White"])
+    params = scraper.get_params(3, "White")
     true_params = (
-            ('page', '3'),
-            ('pageSize', '200'),
-            ('state', 'US-CA'),
-            ('shoppingMethod', 'INSTORE_PICKUP^%^2CDELIVERY'),
-            ('userShoppingMethod', 'INSTORE_PICKUP'),
-            ('allStoresCount', 'true'),
-            ('searchAllStores', 'true'),
-            ('storeId', '1108'),
-            ('department', 'Wine'),
-            ('producttype', 'White^%^20Wine'),
-        )
+        ("page", "3"),
+        ("pageSize", "200"),
+        ("state", "US-CA"),
+        ("shoppingMethod", "INSTORE_PICKUP^%^2CDELIVERY"),
+        ("userShoppingMethod", "INSTORE_PICKUP"),
+        ("allStoresCount", "true"),
+        ("searchAllStores", "true"),
+        ("storeId", "1108"),
+        ("department", "Wine"),
+        ("producttype", "White^%^20Wine"),
+    )
     assert params == true_params
 
 
 def test_get_response():
-    scraper = Scraper(199, ['Red'])
-    response = scraper.get_response(2, 'Red')
+    scraper = Scraper(199, ["Red"])
+    response = scraper.get_response(2, "Red")
     assert response.status_code == 200
--- totalwine_scraper/scraper.py
+++ totalwine_scraper/scraper.py
@@ -14,7 +14,8 @@ class Scraper:
     
     def __init__(self, number_of_items: int, item_types: list) -> None:
         """
-        This scrapes totalwine.com for wine listings and is able to take demanded number of listings and a list of different types of wines to scrape.
+        This scrapes totalwine.com for wine listings
+        It is able to take demanded number of listings and a list of different types of wines to scrape.
         Returns a dataframe or csv file with tabular data.
 
         Args:
@@ -26,70 +27,65 @@ class Scraper:
         self.__item_types = ["".join(item.split()).title() for item in item_types]
         self.items_per_page = 200
 
-
     @property
     def number_of_items(self) -> int:
         return self.__number_of_items
 
-    
     @property
     def item_types(self) -> list:
         return self.__item_types
-
 
     @property
     def number_of_pages(self) -> int:
         return math.ceil(self.__number_of_items / self.items_per_page)
 
-
     @property
     def items_to_return(self) -> int:
         return len(self.__item_types) * self.__number_of_items
 
-
     @staticmethod
     def get_headers(page: int, wine_type: str) -> dict:
         headers = {
-            'authority': 'www.totalwine.com',
-            'sec-ch-ua': '^\\^',
-            'accept': 'application/json, text/plain, */*',
-            'sec-ch-ua-mobile': '?0',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-dest': 'empty',
-            'referer': f'https://www.totalwine.com/wine/c/c0020?&page={(page - 1)}&pageSize=200&department=Wine&producttype={wine_type}^%^20Wine&aty=1,1,0,1',
-            'accept-language': 'en-US,en;q=0.9',
+            "authority": "www.totalwine.com",
+            "sec-ch-ua": "^\\^",
+            "accept": "application/json, text/plain, */*",
+            "sec-ch-ua-mobile": "?0",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+            "sec-fetch-site": "same-origin",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-dest": "empty",
+            "referer": f"https://www.totalwine.com/wine/c/c0020?&page={(page - 1)}&pageSize=200&department=Wine&producttype={wine_type}^%^20Wine&aty=1,1,0,1",
+            "accept-language": "en-US,en;q=0.9",
         }
         return headers
-
 
     @staticmethod
     def get_params(page: int, wine_type: str) -> Set:
         params = (
-            ('page', f'{page}'),
-            ('pageSize', '200'),
-            ('state', 'US-CA'),
-            ('shoppingMethod', 'INSTORE_PICKUP^%^2CDELIVERY'),
-            ('userShoppingMethod', 'INSTORE_PICKUP'),
-            ('allStoresCount', 'true'),
-            ('searchAllStores', 'true'),
-            ('storeId', '1108'),
-            ('department', 'Wine'),
-            ('producttype', f'{wine_type}^%^20Wine'),
+            ("page", f"{page}"),
+            ("pageSize", "200"),
+            ("state", "US-CA"),
+            ("shoppingMethod", "INSTORE_PICKUP^%^2CDELIVERY"),
+            ("userShoppingMethod", "INSTORE_PICKUP"),
+            ("allStoresCount", "true"),
+            ("searchAllStores", "true"),
+            ("storeId", "1108"),
+            ("department", "Wine"),
+            ("producttype", f"{wine_type}^%^20Wine"),
         )
         return params
 
-
-    def get_response(self, page: int, wine_type: str) -> Union[requests.models.Response, str]:
+    def get_response(
+        self, page: int, wine_type: str
+    ) -> Union[requests.models.Response, str]:
         try:
             response = requests.get(
-                f'https://www.totalwine.com/search/api/product/categories/v2/categories/c0020/products?page={page}&pageSize=200&state=US-CA&shoppingMethod=INSTORE_PICKUP%2CDELIVERY&userShoppingMethod=INSTORE_PICKUP&allStoresCount=true&searchAllStores=true&storeId=1108&department=Wine&producttype={wine_type}%20Wine',
+                f"https://www.totalwine.com/search/api/product/categories/v2/categories/c0020/products?page={page}&pageSize=200&state=US-CA&shoppingMethod=INSTORE_PICKUP%2CDELIVERY&userShoppingMethod=INSTORE_PICKUP&allStoresCount=true&searchAllStores=true&storeId=1108&department=Wine&producttype={wine_type}%20Wine",
                 headers=self.get_headers(page, wine_type),
-                params=self.get_params(page, wine_type)
-                )
+                params=self.get_params(page, wine_type),
+            )
             response.raise_for_status()
-            
+
             if response.status_code == 200:
                 return response
 
@@ -101,7 +97,6 @@ class Scraper:
             print("Timeout Error:", errt)
         except requests.exceptions.RequestException as err:
             print("OOps: Something Else", err)
-
 
     def get_json(self, page: int, wine_type: str) -> Dict[str, Any]:
         """
@@ -117,69 +112,62 @@ class Scraper:
         except ValueError as e:
             print(e)
 
-
     def get_products(self, page: int, wine_type: str) -> Dict[str, Any]:
         """
         Checks if json file contains 200 items/page for each wine type and returns those products.
         """
         try:
-            products = self.get_json(page, wine_type)['products']
+            products = self.get_json(page, wine_type)["products"]
             if len(products) == 200:
                 return products
         except:
             print("Corrupted json!")
 
-
     @staticmethod
     def get_brand(item: Dict[str, Any], id: int, brands: list) -> list:
         try:
-            brands.extend([item[id]['brand']['name']])
+            brands.extend([item[id]["brand"]["name"]])
         except:
             brands.extend([None])
         return brands
-    
 
     @staticmethod
     def get_star_average(item: Dict[str, Any], id: int, stars_average: list) -> list:
-        try: 
-            stars_average.extend([item[id]['customerAverageRating']])
+        try:
+            stars_average.extend([item[id]["customerAverageRating"]])
         except:
             stars_average.extend([None])
         return stars_average
 
-
     @staticmethod
     def get_review_count(item: Dict[str, Any], id: int, reviews_count: list) -> list:
         try:
-            reviews_count.extend([item[id]['customerReviewsCount']])
+            reviews_count.extend([item[id]["customerReviewsCount"]])
         except:
             reviews_count.extend([None])
         return reviews_count
-    
 
     @staticmethod
     def get_title(item: Dict[str, Any], id: int, titles: list) -> list:
         try:
-            titles.extend([item[id]['name']])
+            titles.extend([item[id]["name"]])
         except:
             titles.extend([None])
         return titles
 
-
     @staticmethod
     def get_production_year(item: Dict[str, Any], id: int, years: list) -> list:
         try:
-            extract_year = item[id]['name'].split(', ')[1]
+            extract_year = item[id]["name"].split(", ")[1]
             years.extend([int(extract_year)])
         except:
             years.extend([None])
         return years
-    
 
     @staticmethod
     def get_rating_score(item: Dict[str, Any], id: int, rating_scores: list) -> list:
         try:
-            rating = item[id]['rating']
+            rating = item[id]["rating"]
             if rating != 0:
                 rating_scores.extend([rating])
             else:
@@ -188,11 +176,10 @@ class Scraper:
             rating_scores.extend([None])
         return rating_scores
 
-
     @staticmethod
     def get_description(item: Dict[str, Any], id: int, descriptions: list) -> list:
         try:
-            comment = [item[id]['review']]
+            comment = [item[id]["review"]]
             if len(comment) != 0:
                 descriptions.extend(comment)
             else:
@@ -201,12 +188,11 @@ class Scraper:
             descriptions.extend([None])
         return descriptions
 
-
     @staticmethod
     def is_winery_direct(item: Dict[str, Any], id: int, winery_directs: list) -> list:
         try:
-            winery_direct = item[id]['directType']
-            if winery_direct == 'Winery Direct':
+            winery_direct = item[id]["directType"]
+            if winery_direct == "Winery Direct":
                 winery_directs.extend([1])
             else:
                 winery_directs.extend([0])
@@ -214,95 +200,106 @@ class Scraper:
             winery_directs.extend([0])
         return winery_directs
 
-
     @staticmethod
     def get_img_link(item: Dict[str, Any], id: int, img_links: list) -> list:
         try:
-            img_links.extend([item[id]['images'][0]['url']])
+            img_links.extend([item[id]["images"][0]["url"]])
         except:
             img_links.extend([None])
         return img_links
 
-
     @staticmethod
     def get_item_link(item: Dict[str, Any], id: int, links: list) -> list:
         try:
-            full_link = 'https://www.totalwine.com' + item[id]['productUrl']
+            full_link = "https://www.totalwine.com" + item[id]["productUrl"]
             links.extend([full_link])
         except:
             links.extend([None])
         return links
 
-
     @staticmethod
     def get_price(item: Dict[str, Any], id: int, prices: list) -> list:
         try:
-            prices.extend([item[id]['price'][0]['price']])
+            prices.extend([item[id]["price"][0]["price"]])
         except:
             prices.extend([None])
         return prices
 
-
     @staticmethod
     def get_wine_style(item: Dict[str, Any], id: int, item_styles: list) -> list:
         try:
-            item_styles.extend([item[id]['itemStyle']])
+            item_styles.extend([item[id]["itemStyle"]])
         except:
             item_styles.extend([None])
         return item_styles
-    
 
     @staticmethod
     def get_wine_body(item: Dict[str, Any], id: int, bodies: list) -> list:
         try:
-            bodies.extend([item[id]['itemBody']])
+            bodies.extend([item[id]["itemBody"]])
         except:
             bodies.extend([None])
         return bodies
 
-
     @staticmethod
     def get_wine_taste(item: Dict[str, Any], id: int, tastes: list) -> list:
         try:
-            tastes.extend([item[id]['itemTasteProfile']])
+            tastes.extend([item[id]["itemTasteProfile"]])
         except:
             tastes.extend([None])
         return tastes
-    
 
     @staticmethod
     def get_wine_stock(item: Dict[str, Any], id: int, stocks_available: list) -> list:
         try:
-            stocks_available.extend([item[id]['stockLevel'][0]['stock']])
+            stocks_available.extend([item[id]["stockLevel"][0]["stock"]])
         except:
             stocks_available.extend([None])
         return stocks_available
-    
-    
+
     @staticmethod
-    def get_categories(item: Dict[str, Any], id: int, regions: list, wine_types: list, varietals: list, appelations: list, styles: list, countries: list) -> list:
+    def get_categories(
+        item: Dict[str, Any],
+        id: int,
+        regions: list,
+        wine_types: list,
+        varietals: list,
+        appelations: list,
+        styles: list,
+        countries: list,
+    ) -> list:
         """
         Grabs all categories of each item and matches it with a given required categories: region, product_type, varietal_type, appelation, style.
         Returns extended lists for all categories with either grabbed value or None entry.
         """
         try:
-            item_categories = item[id]['categories']
+            item_categories = item[id]["categories"]
 
-            categories = ['REGION', 'PRODUCT_TYPE', 'VARIETAL_TYPE', 'APPELLATION', 'STYLE']
+            categories = [
+                "REGION",
+                "PRODUCT_TYPE",
+                "VARIETAL_TYPE",
+                "APPELLATION",
+                "STYLE",
+            ]
             lists_of_features = [regions, wine_types, varietals, appelations, styles]
 
             item_types, item_type_names, item_country = ([] for i in range(3))
 
             for item in range(len(item_categories)):
 
-                item_types.extend([item_categories[item]['type']])
-                item_type_names.extend([item_categories[item]['name']])
+                item_types.extend([item_categories[item]["type"]])
+                item_type_names.extend([item_categories[item]["name"]])
 
-                if item_categories[item]['type'] == 'REGION':
-                    extracted_country = item_categories[item]['storefrontUrl'].split('/')[2]
+                if item_categories[item]["type"] == "REGION":
+                    extracted_country = item_categories[item]["storefrontUrl"].split(
+                        "/"
+                    )[2]
                     item_country.extend([extracted_country])
 
-            countries.extend(item_country) if len(item_country) != 0 and item_country != ['c'] else countries.extend([None])
+            countries.extend(item_country) if len(
+                item_country
+            ) != 0 and item_country != ["c"] else countries.extend([None])
 
             wine_features_dict = {k: v for k, v in zip(item_types, item_type_names)}
 
@@ -319,9 +316,8 @@ class Scraper:
             appelations.extend([None])
             styles.extend([None])
             countries.extend([None])
-        
-        return regions, wine_types, varietals, appelations, styles, countries
 
+        return regions, wine_types, varietals, appelations, styles, countries
 
     def scrape_items(self) -> pd.DataFrame:
         """
@@ -332,7 +328,29 @@ class Scraper:
             pd.DataFrame: tabular data with all scraped listings and its features.
         """
 
-        titles, rating_scores, prices, years, img_links, links, winery_directs, descriptions, brands, countries, regions, appelations, wine_types, varietals, styles, tastes, bodies, item_styles, stars_average, reviews_count, stocks_available = ([] for _ in range(21))
+        (
+            titles,
+            rating_scores,
+            prices,
+            years,
+            img_links,
+            links,
+            winery_directs,
+            descriptions,
+            brands,
+            countries,
+            regions,
+            appelations,
+            wine_types,
+            varietals,
+            styles,
+            tastes,
+            bodies,
+            item_styles,
+            stars_average,
+            reviews_count,
+            stocks_available,
+        ) = ([] for _ in range(21))
 
         item_types = self.item_types
 
@@ -345,67 +363,60 @@ class Scraper:
                 for id in range(self.items_per_page):
 
                     self.get_title(item, id, titles)
-
                     self.get_production_year(item, id, years)
-
                     self.get_price(item, id, prices)
-
                     self.get_star_average(item, id, stars_average)
-
                     self.get_review_count(item, id, reviews_count)
-                    
                     self.get_rating_score(item, id, rating_scores)
-
                     self.is_winery_direct(item, id, winery_directs)
-
                     self.get_wine_stock(item, id, stocks_available)
-
                     self.get_brand(item, id, brands)
-
-                    self.get_categories(item, id, regions, wine_types, varietals, appelations, styles, countries)
-
                     self.get_wine_style(item, id, item_styles)
-
                     self.get_wine_taste(item, id, tastes)
-
                     self.get_wine_body(item, id, bodies)
-
                     self.get_description(item, id, descriptions)
-
                     self.get_img_link(item, id, img_links)
-
                     self.get_item_link(item, id, links)
-                
+                    self.get_categories(
+                        item,
+                        id,
+                        regions,
+                        wine_types,
+                        varietals,
+                        appelations,
+                        styles,
+                        countries,
+                    )
+
                 time.sleep(np.random.uniform(2, 7))
-            
+
         dataframe = pd.DataFrame(
             {
-                'title': titles,
-                'year': years,
-                'price': prices,
-                'c_rating': stars_average,
-                'reviews': reviews_count,
-                'score': rating_scores,
-                'winery_direct': winery_directs,
-                'stock': stocks_available,
-                'brand': brands,
-                'country/state': countries,
-                'region': regions,
-                'appelation': appelations,
-                'wine_type': wine_types,
-                'varietal': varietals,
-                'varietal_style': styles,
-                'style': item_styles,
-                'taste': tastes,
-                'body': bodies,
-                'description': descriptions,
-                'img': img_links,
-                'link': links
-                }
-                )
-        
-        return dataframe.iloc[:self.items_to_return]
+                "title": titles,
+                "year": years,
+                "price": prices,
+                "c_rating": stars_average,
+                "reviews": reviews_count,
+                "score": rating_scores,
+                "winery_direct": winery_directs,
+                "stock": stocks_available,
+                "brand": brands,
+                "country/state": countries,
+                "region": regions,
+                "appelation": appelations,
+                "wine_type": wine_types,
+                "varietal": varietals,
+                "varietal_style": styles,
+                "style": item_styles,
+                "taste": tastes,
+                "body": bodies,
+                "description": descriptions,
+                "img": img_links,
+                "link": links,
+            }
+        )
 
+        return dataframe.iloc[: self.items_to_return]
 
     @staticmethod
     def get_csv(df: pd.DataFrame, path: str) -> None:
@@ -416,4 +427,4 @@ class Scraper:
             df (pd.DataFrame): dataframe with the scraped data.
             path (str): location on the device to save created csv.
         """
-        return df.to_csv(os.path.join(path,r'totalwine_data.csv'))
+        return df.to_csv(os.path.join(path, r"totalwine_data.csv"))
